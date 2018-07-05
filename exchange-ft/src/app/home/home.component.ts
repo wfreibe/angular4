ea2db4d68821cb7a3016e63cb5da25452a58d0bf
Werner Freiberger <wfreiberger@nemetschek.com>
2018-07-05 14:39:23 +0200

    added first org selection

--- exchange-ft/src/app/home/home.component.ts
+++ exchange-ft/src/app/home/home.component.ts
@@ -6,11 +6,11 @@ import { AuthService} from '../auth/auth.service';
 @Component({
   selector: 'exc-home',
   template: `
-    <div class="ui container two column grid">
-      <div class="ui container text">
-        <h1 class="ui header">Exchange FT</h1>
-        <p>Die digitale Dokumentenmanagement-Applikation. Jetzt noch schneller und intuitiver.</p>
-        <p>Diese Projektstudie verwendet das Angular- und Lumenframework.</p>
+
+      <div class="ui text container">
+        <h1 class="ui inverted header">EXC FT</h1>
+        <h2>Die digitale Dokumentenmanagement-Applikation. Jetzt noch schneller und intuitiver.</h2>
+        <h2>Diese Projektstudie verwendet das Angular- und Lumenframework.</h2>
         <a routerLink="../users" *ngIf="auth.isAuthenticated()" class="ui red button">
           Userliste ansehen
           <i class="right arrow icon"></i>
@@ -18,7 +18,7 @@ import { AuthService} from '../auth/auth.service';
         <br><br>
         <exc-search *ngIf="auth.isAuthenticated()" (userSelected)="userSelected($event)" class="column"></exc-search>
       </div>
-    </div>
+
   `
 })
 
